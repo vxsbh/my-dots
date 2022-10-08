9711b6c1a08640e359676867e1c7a24af04a8bed
Antoine <antoine.lepine@ymail.com>
2022-10-08 22:49:58 +0200

    modified bar name in launch script

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -6,7 +6,7 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1
+# Launch mainbar-i3
 echo "---" | tee -a /tmp/polybar1.log
 polybar mainbar-i3 2>&1 | tee -a /tmp/polybar1.log & disown
 
