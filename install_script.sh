9dfeed41aea3649a56b97538d3f15d5dbfff6570
Antoine <antoine.lepine@ymail.com>
2022-10-15 14:56:14 +0200

    minor details update

--- install_script.sh
+++ install_script.sh
@@ -5,19 +5,24 @@ read -p "Do you want to install the required software? [O/n] " choice
 
 CHOICE=$(echo $choice | tr '[:lower:]' '[:upper:]') #converts all characters to uppercase
 
-if [ "$CHOICE" == 'O' ]
+menu () {
+    if [ "$CHOICE" == 'O' ]
 then
     read -p "How do you install software on your distro? (ex: pacman -S / dnf install / apt install) " distro
     sudo $distro i3-gaps polybar picom dunst rofi conky nitrogen flatpak
 
 elif [ "$CHOICE" == 'N' ]
 then
-    echo "Exiting script..."
+    echo "Bye bye... (exiting script)"
     exit
 else
-    echo "Didn’t understand your choice, exiting script..."
+    echo "Didn’t understand your choice... (exiting script)"
     exit
 fi
+}
+
+menu
+
 
 # made by antoine, with love 
 # 15/10/2022
